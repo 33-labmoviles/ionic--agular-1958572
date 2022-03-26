6763c9e49b77394f4d09b65b72ca43a817fe7651
Kevin <kevingatikha11@gmail.com>
2022-03-26 02:06:13 -0600

    26-feb

--- ionic-app/Ionic-angular-Practica-3/src/app/tab1/tab1.page.ts
+++ ionic-app/Ionic-angular-Practica-3/src/app/tab1/tab1.page.ts
@@ -9,60 +9,60 @@ export class Tab1Page {
 
   constructor() {}
 
-    Alumnos = [
+    alumnos = [
       {
         "Nombre": "Kevin",
         "Apellidos": "Gatica Vega",
         "Matricula": "1958572",
-        "Image": "assets/icon/gato.jpng"
+        "Image": "assets/icon/favicon.png"
       },
       {
         "Nombre": "Marcelo",
         "Apellidos": "Reyes",
         "Matricula": "1234567",
-        "Image": "assets/icon/paz.jpng"
+        "Image": "assets/icon/favicon.png"
       },
       {
         "Nombre": "Eric",
         "Apellidos": "Rodriguez",
         "Matricula": "7654321",
-        "Image": "assets/icon/perry.png"
+        "Image": "assets/icon/favicon.png"
       },
       {
         "Nombre": "Alberto",
         "Apellidos": "Flores",
         "Matricula": "11111111",
-        "Image": "assets/icon/reloj.png"
+        "Image": "assets/icon/favicon.png"
       },
       {
         "Nombre": "Diego",
         "Apellidos": "Davila",
         "Matricula": "3331112",
-        "Image": "assets/icon/gato.png"
+        "Image": "assets/icon/favicon.png"
       },
       {
         "Nombre": "Luis",
         "Apellidos": "Otoñiel",
         "Matricula": "1671234",
-        "Image": "assets/icon/perry.png"
+        "Image": "assets/icon/favicon.png"
       },
       {
         "Nombre": "Emiliano",
         "Apellidos": "Zapata",
         "Matricula": "6666666",
-        "Image": "assets/icon/reloj.png"
+        "Image": "assets/icon/favicon.png"
       },
       {
         "Nombre": "Mario",
         "Apellidos": "Bros",
         "Matricula": "1234512",
-        "Image": "assets/icon/paz.png"
+        "Image": "assets/icon/favicon.png"
       },
       {
         "Nombre": "Pana",
         "Apellidos": "Miguel",
         "Matricula": "3433321",
-        "Image": "assets/icon/paz.png"
+        "Image": "assets/icon/favicon.png"
       }
     ];
 
